521eeb1974b72009f9c2ad0a2dee7eb0fad8f5b8
p.fol <pavel.fol@roguewave.com>
2018-08-30 15:03:03 +0200

    add execution of Owner test

--- src/test/groovy/integration/ITTestSuite.groovy
+++ src/test/groovy/integration/ITTestSuite.groovy
@@ -1,5 +1,6 @@
 package integration
 
+import integration.test.ZOwners
 import integration.test.ZSupplements
 import integration.test.ZMain
 import org.junit.runner.RunWith
@@ -7,7 +8,7 @@ import org.junit.runners.Suite
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses([
-        ZMain.class, ZSupplements.class
+        ZMain.class, ZSupplements.class, ZOwners.class
 ])
 
 public class ITTestSuite {
